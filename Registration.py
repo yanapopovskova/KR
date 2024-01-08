ba9265f334a42ac01867a0c6cd8c4dc56d4a08b9
yanapopovskova <124494674+yanapopovskova@users.noreply.github.com>
2024-01-08 19:34:13 +0400

    Update Registration.py

--- Registration.py
+++ Registration.py
@@ -1,105 +1,102 @@
-from PyQt5.Qt import *
-from shifr import *
+from tkinter import *
+import tkinter.messagebox as messagebox
+import os
 
-class Ui_signUp(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(570, 375)
-        self.label = QLabel(Dialog)
-        self.label.setGeometry(QRect(160, 130, 81, 31))
-        font = QFont()
-        font.setPointSize(10)
-        self.label.setFont(font)
-        self.label.setObjectName("label")
-        self.label_2 = QLabel(Dialog)
-        self.label_2.setGeometry(QRect(160, 230, 81, 31))
-        font = QFont()
-        font.setPointSize(10)
-        self.label_2.setFont(font)
-        self.label_2.setObjectName("label_2")
-        self.label_3 = QLabel(Dialog)
-        self.label_3.setGeometry(QRect(160, 180, 81, 31))
-        font = QFont()
-        font.setPointSize(10)
-        self.label_3.setFont(font)
-        self.label_3.setObjectName("label_3")
-        self.uname_lineEdit = QLineEdit(Dialog)
-        self.uname_lineEdit.setGeometry(QRect(250, 130, 141, 20))
-        self.uname_lineEdit.setObjectName("uname_lineEdit")
-        self.email_lineEdit = QLineEdit(Dialog)
-        self.email_lineEdit.setGeometry(QRect(250, 180, 141, 20))
-        self.email_lineEdit.setObjectName("email_lineEdit")
-        self.password_lineEdit = QLineEdit(Dialog)
-        self.password_lineEdit.setGeometry(QRect(250, 230, 141, 20))
-        self.password_lineEdit.setObjectName("password_lineEdit")
-        self.signup_btn = QPushButton(Dialog)
-        self.signup_btn.setGeometry(QRect(270, 290, 75, 23))
-        self.signup_btn.setObjectName("signup_btn")
-        self.label_4 = QLabel(Dialog)
-        self.label_4.setGeometry(QRect(150, 10, 321, 81))
-        font = QFont()
-        font.setPointSize(18)
-        self.label_4.setFont(font)
-        self.label_4.setAlignment(Qt.AlignCenter)
-        self.label_4.setObjectName("label_4")
+def register_window():
+    root.withdraw()  # Скрыть основное окно
+    register_window = Toplevel(root)
+    register_window.title("Регистрация")
 
-        self.retranslateUi(Dialog)
-        QMetaObject.connectSlotsByName(Dialog)
+    register_window_width = 300
+    register_window_height = 300
 
-    def retranslateUi(self, Dialog):
-        _translate = QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Регистрация"))
-        self.label.setText(_translate("Dialog", "Логин"))
-        self.label_2.setText(_translate("Dialog", "Пароль"))
-        self.label_3.setText(_translate("Dialog", "Email"))
-        self.signup_btn.setText(_translate("Dialog", "Войти"))
-        self.label_4.setText(_translate("Dialog", "Регистрация"))
+    register_window_x = int((screen_width / 2) - (register_window_width / 2))
+    register_window_y = int((screen_height / 2) - (register_window_height / 2))
+
+    register_window.geometry(f"{register_window_width}x{register_window_height}+{register_window_x}+{register_window_y}")
+
+    label_register_username = Label(register_window, text="Логин:", font=("Arial", 12))
+    label_register_username.pack()
+    entry_register_username = Entry(register_window)
+    entry_register_username.pack()
+
+    label_register_password = Label(register_window, text="Пароль:", font=("Arial", 12))
+    label_register_password.pack()
+    entry_register_password = Entry(register_window, show="*")
+    entry_register_password.pack()
+
+    label_register_confirm_password = Label(register_window, text="Подтверждение пароля:",  font=("Arial", 12))
+    label_register_confirm_password.pack()
+    entry_register_confirm_password = Entry(register_window, show="*")
+    entry_register_confirm_password.pack()
+
+    button_register = Button(register_window, text="Регистрация",  font=("Arial", 12), command=lambda: register(entry_register_username.get(), entry_register_password.get(), entry_register_confirm_password.get(), register_window))
+    button_register.pack()
 
 
-class Dialog(QDialog, Ui_signUp):
-    def __init__(self, parent=None):
-        super(Dialog, self).__init__(parent)
-        self.setupUi(self)
-        self.parent = parent
+def register(username, password, confirm_password, register_window):
+    if username == "" or password == "" or confirm_password == "":
+        messagebox.showerror("Ошибка", "Пожалуйста, заполните все поля")
+        return
 
-        self.signup_btn.clicked.connect(self.insertData)
+    if password != confirm_password:
+        messagebox.showerror("Ошибка", "Пароли не совпадают")
+        return
 
-    @pyqtSlot()
-    def insertData(self):
-        
+    with open("data.txt", "a") as file:
+        file.write(f"{username},{password}\n")
 
-        username = trippledesencrypt(self.uname_lineEdit.text())
-        email = trippledesencrypt(self.email_lineEdit.text())
-        password = trippledesencrypt(self.password_lineEdit.text())
-
-        file = open('login.txt', 'r', encoding='utf-8')
-        text = file.readlines()
-        file.close()
-
-        if (not username) or (not email) or (not password):
-            msg = QMessageBox.information(self, 'Внимание!', 'Вы не заполнили все поля.')
-            return
-
-        flag = True
-        for i in range(len(text)):
-            text[i] = text[i][:-1]
-            row = text[i].split(" ")
-            if username == row[0]:
-                msg = QMessageBox.information(self, 'Внимание!', 'Пользоватеть с таким именем уже зарегистрирован.')
-                flag = False
-                break
-            
-        if flag:
-            # if log != row[0] and email != row[1]:
-            file = open('login.txt', 'a', encoding='utf-8')
-            file.write('\n' + username + ' ' + email + ' ' + password)
-            file.close()
-            self.close()
+    messagebox.showinfo("Регистрация", "Регистрация прошла успешно")
+    register_window.destroy()
+    root.deiconify()  # Открыть окно входа
 
 
-if __name__ == "__main__":
-    import sys
-    app    = QApplication(sys.argv)
-    w = Dialog()
-    w.show()
-    sys.exit(app.exec_())
+def login(username, password):
+    if username == "" or password == "":
+        messagebox.showerror("Ошибка", "Пожалуйста, заполните все поля")
+        return
+
+    if not os.path.isfile("data.txt"):
+        messagebox.showerror("Ошибка", "Неверное имя пользователя или пароль")
+        return
+
+    with open("data.txt", "r") as file:
+        for line in file:
+            stored_username, stored_password = line.strip().split(",")
+            if username == stored_username and password == stored_password:
+                messagebox.showinfo("Вход", "Вход выполнен")
+                root.destroy()  # Закрыть окно входа
+                return
+
+    messagebox.showerror("Ошибка", "Неверное имя пользователя или пароль")
+
+root = Tk()
+root.title("Вход")
+
+label_login_username = Label(root, text="Логин:", font=("Arial", 12))
+label_login_username.pack()
+entry_login_username = Entry(root)
+entry_login_username.pack()
+
+label_login_password = Label(root, text="Пароль:", font=("Arial", 12))
+label_login_password.pack()
+entry_login_password = Entry(root, show="*")
+entry_login_password.pack()
+
+button_login = Button(root, text="Вход",  font=("Arial", 12), command=lambda: login(entry_login_username.get(), entry_login_password.get()))
+button_login.pack()
+
+button_register = Button(root, text="Регистрация",  font=("Arial", 12), command=register_window)
+button_register.pack()
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+x = int((screen_width / 2) - (200 / 2))
+y = int((screen_height / 2) - (200 / 2))
+
+root.geometry(f"300x300+{x}+{y}")
+
+previous_register_window = None
+
+root.mainloop()
